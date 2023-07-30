147505d51feece4f3f2a17fdab8e8828b93d959b
Lai Khim Soon <khimsoon_lai@hotmail.com>
2023-07-31 02:22:29 +0800

    styled code

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,10 +1,12 @@
 import sqlite3
 import logging
 
-from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
+from flask import Flask, jsonify, json, render_template, request, url_for
+from flask import redirect, flash
 from werkzeug.exceptions import abort
 
 db_connection_count = [0]
+
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
@@ -14,21 +16,23 @@ def get_db_connection():
     db_connection_count[0] += 1
     return connection
 
+
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+                              (post_id,)).fetchone()
     connection.close()
     db_connection_count[0] -= 1
     return post
+
 
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
 
-# Define the main route of the web application 
+# Define the main route of the web application
 @app.route('/')
 def index():
     connection = get_db_connection()
@@ -37,16 +41,18 @@ def index():
     db_connection_count[0] -= 1
     return render_template('index.html', posts=posts)
 
-# Define how each individual article is rendered 
+
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      app.logger.info('Request unsuccessful!')
-      return render_template('404.html'), 404
+        app.logger.info('Request unsuccessful!')
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        return render_template('post.html', post=post)
+
 
 # Define the About Us page
 @app.route('/about')
@@ -54,7 +60,8 @@ def about():
     app.logger.info('Request for "About Us" page successful!')
     return render_template('about.html')
 
-# Define the post creation functionality 
+
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -66,8 +73,9 @@ def create():
             flash('Title is required!')
         else:
             connection = get_db_connection()
-            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+            connection.execute(
+                        'INSERT INTO posts (title, content) VALUES (?, ?)',
+                        (title, content))
             connection.commit()
             connection.close()
             db_connection_count[0] -= 1
@@ -75,7 +83,6 @@ def create():
             app.logger.info(f'Article "{title}" posted successful!')
             return redirect(url_for('index'))
 
-        
     return render_template('create.html')
 
 
@@ -83,7 +90,7 @@ def create():
 @app.route('/healthz')
 def healtz_status():
     response = app.response_class(
-            response=json.dumps({"result":"OK - healthy"}),
+            response=json.dumps({"result": "OK - healthy"}),
             status=200,
             mimetype='application/json'
     )
@@ -97,7 +104,8 @@ def metrics():
     connection = get_db_connection()
     post_count = connection.execute('SELECT COUNT(*) FROM posts').fetchone()[0]
     response = app.response_class(
-            response=json.dumps({"db_connection_count": db_connection_count[0], "post_count": post_count}),
+            response=json.dumps({"db_connection_count": db_connection_count[0],
+                                "post_count": post_count}),
             status=200,
             mimetype='application/json'
     )
@@ -105,8 +113,9 @@ def metrics():
     db_connection_count[0] -= 1
     app.logger.info('Request for matrics successful!')
     return response
-    
+
+
 # start the application on port 3111
 if __name__ == "__main__":
-   logging.basicConfig(filename='app.log',level=logging.DEBUG)
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+    app.run(host='0.0.0.0', port='3111')
